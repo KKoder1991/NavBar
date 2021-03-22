455ca7e71e89e030a44bbeb877b55d139924c882
KKoder1991 <konondlovu@icloud.com>
2021-03-22 19:03:44 +0100

    Add variables for game in logic file

---
+++ rockpaperscissors/logic.js
@@ -0,0 +1,7 @@
+const userScore = 0;
+const computerScore = 0;
+
+/*These are variables for DOM variables to use for the DOM elements */
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+
