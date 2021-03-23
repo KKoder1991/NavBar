4ed4f42bac9c86a405a0d9183aead1e2603072e0
KKoder1991 <konondlovu@icloud.com>
2021-03-23 10:00:19 +0100

    Write function to pass in computers choice

--- rockpaperscissors/logic.js
+++ rockpaperscissors/logic.js
@@ -4,8 +4,8 @@ const userScore = 0;
 const computerScore = 0;
 
 /*These are variables for DOM variables to use for the DOM elements */
-const userScore_span = document.getElementById('user-score');
-const computerScore_span = document.getElementById('computer-score');
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 
 /*Used to cache the DOM and make it easier to write the code in the program.
 We are storing something for future use. Storing things in variables to use later.*/
@@ -15,17 +15,27 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissor_div = document.getElementById("s");
 
+function computerChoice() {
+    const choices = ["r", "p", "s"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+console.log(computerChoice());
+
+
+
 
 function main(){
     rock_div.addEventListener('click', function(){
         game("r");
     })
 
-    rock_div.addEventListener('click', function(){
+    paper_div.addEventListener('click', function(){
         game("p");
     })
 
-    rock_div.addEventListener('click', function(){
+    scissor_div.addEventListener('click', function(){
         game("s");
     })
 }
