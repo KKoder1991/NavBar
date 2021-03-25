eeccabb934b7981bde39cf3b9a3d60065b1e2b2d
KKoder1991 <konondlovu@icloud.com>
2021-03-25 12:57:32 +0100

    Add main logic for game switch statements

--- rockpaperscissor2/logic.js
+++ rockpaperscissor2/logic.js
@@ -2,17 +2,43 @@
 let userScore = 0;
 let computerScore = 0;
 
+
+/*This function will generate a random choice for the computer. I used math.random and set it 
+to get a float number up to 3. Math.floor then rounds down the number. the randomChoice
+function is then passed into the choices array which then makes a random choice between the
+three avalaible choices.*/
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
-    const randomChoice = Math.floor(Math.random * 3);
-        return choices[randomChoice]
+    const randomChoice = Math.floor(Math.random() * 3);
+        return choices[randomChoice];
 }
 
-console.log(getComputerChoice())
+function game() {
+    const computerChoice = getComputerChoice();
+    switch(userChoice + computerChoice) {
+        case "rockscissor":
+        case "paperrock":
+        case "scissorpaper":
+            console.log("You have won!");
+            break;
+        case "rockscissor":
+        case "paperrock":
+        case "scissorpaper":
+            console.log("You have lost");
+            break;
+        case "rockrock":
+        case "paperpaper":
+        case "scissorscissor":
+            console.log("It is a draw");
+            break;
+
+    }
+
+}
 
 /* This gets the different elements in the HTML document, each element is represented 
 by the element id i.e. "rock". An anomous function is passed through, which calls the function game."*/
-function main {
+function main() {
     document.getElementById("rock").addEventListener('click', () => game("rock"));
     document.getElementById("paper").addEventListener('click', () => game("paper"));
     document.getElementById("paper").addEventListener('click', () => game("scissor"));
