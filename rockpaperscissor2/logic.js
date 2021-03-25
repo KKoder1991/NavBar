3faabbe26eb42807d3c64431ae210df081da6f1b
KKoder1991 <konondlovu@icloud.com>
2021-03-25 15:46:48 +0100

    add win, lose, draw func $ main statements @ end

--- rockpaperscissor2/logic.js
+++ rockpaperscissor2/logic.js
@@ -13,30 +13,45 @@ function getComputerChoice() {
         return choices[randomChoice];
 }
 
+function win(userChoice, computerChoice){
+    userScore ++;
+    document.getElementById("user-score").innerHTML = userScore;
+    document.querySelector(".result > p").innerHTML = `${userChoice} beats ${computerChoice}, you win!`;
+
+}
+
+function lose(userChoice, computerChoice){
+    computerScore ++;
+    document.getElementById("computer-score").innerHTML = computerScore;
+    document.querySelector(".result > p").innerHTML = `${computerChoice} beats ${userChoice}, you lose!`
+}
+
+function draw(userChoice, computerChoice){
+    document.querySelector(".result > p").innerHTML = `${userChoice} and ${computerChoice} are equal. It is a tie!`;
+}
+
 /*The game function looks at the 9 different outcomes the game could have.  the switch case
 starts by combining the user and computer choices. After the choices are made, it is combined to see
 what outcome there is. After each set of win, lose or draw outcomes a message is printed to the console.*/
-function game() {
+function game(userChoice) {
     const computerChoice = getComputerChoice();
     switch(userChoice + computerChoice) {
         case "rockscissor":
         case "paperrock":
         case "scissorpaper":
-            console.log("You have won!");
+            win(userChoice, computerChoice);
             break;
-        case "rockscissor":
-        case "paperrock":
-        case "scissorpaper":
-            console.log("You have lost");
+        case "scissorrock":
+        case "rockpaper":
+        case "paperscissor":
+            lose(userChoice, computerChoice);
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorscissor":
-            console.log("It is a draw");
+            draw(userChoice, computerChoice);
             break;
-
     }
-
 }
 
 /* This gets the different elements in the HTML document, each element is represented 
@@ -44,5 +59,7 @@ by the element id i.e. "rock". An anomous function is passed through, which call
 function main() {
     document.getElementById("rock").addEventListener('click', () => game("rock"));
     document.getElementById("paper").addEventListener('click', () => game("paper"));
-    document.getElementById("paper").addEventListener('click', () => game("scissor"));
+    document.getElementById("scissor").addEventListener('click', () => game("scissor"));
 } 
+
+main();
