ebd44ddf6f3d981433fdd1bb5ac6fce2bf8e0239
KKoder1991 <konondlovu@icloud.com>
2021-03-25 11:59:13 +0100

    Add logic changes to the javascript file.

--- rockpaperscissor2/logic.js
+++ rockpaperscissor2/logic.js
@@ -10,6 +10,8 @@ function getComputerChoice() {
 
 console.log(getComputerChoice())
 
+/* This gets the different elements in the HTML document, each element is represented 
+by the element id i.e. "rock". An anomous function is passed through, which calls the function game."*/
 function main {
     document.getElementById("rock").addEventListener('click', () => game("rock"));
     document.getElementById("paper").addEventListener('click', () => game("paper"));
